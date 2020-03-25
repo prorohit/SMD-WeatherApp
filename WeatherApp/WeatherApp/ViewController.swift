0f830f76698f0f5d6a81b4ae246dce6faae8fe61
Rohit Singh <rosingh@mafcarrefour.com>
2020-03-25 21:43:55 +0400

    Initial commit of project setup

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Rohit Singh on 25/03/2020.
+//  Copyright © 2020 SMD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
